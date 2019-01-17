74aa3db4fcf91ba36ce0c8e04b0607780278d4bd
Daniel Vega <danve4@gmail.com>
2019-01-16 20:17:11 -0600

    Added urls for each CTA API request

--- node/bus.js
+++ node/bus.js
@@ -1,15 +1,27 @@
-exports.directions = (req, res) => {
+const request = require('request');
+const config = require('./config.json');
 
+exports.routes = (req, res) => {
+  const url = config.bus.baseURL + '/getroutes?key=' + config.bus.APIKey + '&format=json';
+};
+
+exports.directions = (req, res) => {
+  const route = req.query.route;
+  const url = config.bus.baseURL + '/getdirections?key=' + config.bus.APIKey + '&rt=' + route + '&format=json';
 }
 
 exports.routeStops = (req, res) => {
-
+  const route = req.query.route;
+  const direction = req.query.direction;
+  const url = config.bus.baseURL + '/getstops?key=' + config.bus.APIKey + '&rt=' + route + '&dir=' + direction + '&format=json';
 }
 
 exports.stopArrivals = (req, res) => {
-
+  const stopId = req.query.stopId;
+  const url = config.bus.baseURL + '/getpredictions?key=' + config.bus.APIKey + '&stpid=' + stopId + '&format=json';
 }
 
 exports.follow = (req, res) => {
-
+  const vehicleId = req.query.vehicleId;
+  const url = config.bus.baseURL + '/getpredictions?key=' + config.bus.APIKey + '&vid=' + vehicleId + '&format=json';
 }
