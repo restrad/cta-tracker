68939fa0beb4faa30d21cef3f1c4c11884a2f788
restrada25 <restrad2@hawk.iit.edu>
2019-01-16 18:31:02 -0600

    Adding node express boiler plate

---
+++ node/app.js
@@ -0,0 +1,26 @@
+const request = require('request');
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use((req, res, next) => {
+  res.set('Access-Control-Allow-Origin', "*");
+  res.set('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Cache-Control');
+  req.method === 'OPTIONS' ? res.sendStatus(200) : next();
+});
+
+app.get('/cta', function (req, res) {
+  request('http://google.com', function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred and handle it
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    res.send(body);
+  });
+});
+
+app.listen(app.get('port'), () => {
+  console.log('Node app is running on port', app.get('port'));
+});
