604ee7885471b15b71030dcee7176c16ba0a5981
restrada25 <restrad2@hawk.iit.edu>
2019-01-16 20:17:30 -0600

    Added urls for each CTA API request

--- node/train.js
+++ node/train.js
@@ -1,7 +1,12 @@
-exports.stops = (req, res) => {
+const request = require('request');
+const config = require('./config.json');
 
+exports.stops = (req, res) => {
+  const mapId = req.query.stopId;
+  const url = config.train.baseURL+'/ttarrivals.aspx?key='+config.train.APIKey+'&mapId='+mapId+'&outputType=JSON';
 };
 
 exports.follow = (req, res) => {
-
+  const runNumber = req.query.vehicleId;
+  const url = config.train.baseURL+'/ttfollow.aspx?key='+config.train.APIKey+'&runnumber='+runNumber+'&outputType=JSON';
 };
