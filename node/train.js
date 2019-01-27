f373d4ef6e592860cc3584c3ba61a32ddaad05a2
restrada25 <restrad2@hawk.iit.edu>
2019-01-27 17:37:04 -0600

    Implementing requests

--- node/train.js
+++ node/train.js
@@ -4,9 +4,20 @@ const config = require('./config.json');
 exports.stops = (req, res) => {
   const mapId = req.query.stopId;
   const url = config.train.baseURL+'/ttarrivals.aspx?key='+config.train.APIKey+'&mapId='+mapId+'&outputType=JSON';
+  commonRequest(url, res);
 };
 
 exports.follow = (req, res) => {
   const runNumber = req.query.vehicleId;
   const url = config.train.baseURL+'/ttfollow.aspx?key='+config.train.APIKey+'&runnumber='+runNumber+'&outputType=JSON';
+  commonRequest(url, res);
 };
+
+function commonRequest(url, res) {
+  res.type('json');
+  request(url, (error, response, body) => {
+    if (error)
+      res.status(503).send('{"error": "Unable to reach CTA services."}');
+    res.send(body);
+  });
+}
