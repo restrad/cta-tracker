c800923c47058845b5d3dd20130d722a4d498f89
Daniel Vega <danve4@gmail.com>
2019-02-09 04:52:17 -0600

    Adding stop listing functionality

--- cta-tracker/src/app/stops/stops.component.ts
+++ cta-tracker/src/app/stops/stops.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BusService } from '../services/bus.service';
+import { BustimeResponse, Stop, Error } from '../busResponse';
+import { of, Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-stops',
@@ -7,9 +12,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StopsComponent implements OnInit {
 
-  constructor() { }
+  forRoute: string;
+  forDirection: string;
+  stops$: Observable<Stop[]>;
+  error$: Observable<Error[]>;
+
+  constructor(private activatedRoute: ActivatedRoute,
+    private busService: BusService) { }
 
   ngOnInit() {
+    this.activatedRoute.paramMap.pipe(switchMap(params => {
+      this.forRoute = params.get('route');
+      this.forDirection = params.get('direction');
+      return this.busService.stops(this.forRoute, this.forDirection);
+    })).subscribe((response: BustimeResponse) => {
+      if (response.error) {
+        this.error$ = of(response.error);
+      } else {
+        this.stops$ = of(response.stops);
+      }
+    });
   }
 
 }
