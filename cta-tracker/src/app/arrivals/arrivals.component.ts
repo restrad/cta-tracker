84daf3168a53aed032d9ed8b26e210eae932383b
Daniel Vega <danve4@gmail.com>
2019-02-11 00:53:25 -0600

    Adding refresh interval and handling when bus is delayed

--- cta-tracker/src/app/arrivals/arrivals.component.ts
+++ cta-tracker/src/app/arrivals/arrivals.component.ts
@@ -18,9 +18,12 @@ export class ArrivalsComponent implements OnInit {
   forStopName: string;
   @Input() vehicles$: Observable<Prd[]>;
   error$: Observable<Error[]>;
+  refreshInterval: number;
 
   constructor(private activatedRoute: ActivatedRoute,
-    private busService: BusService) { }
+    private busService: BusService) {
+    this.refreshInterval = 30 * 1000; // In seconds
+  }
 
   ngOnInit() {
     this.activatedRoute.paramMap.pipe(switchMap(params => {
@@ -30,18 +33,49 @@ export class ArrivalsComponent implements OnInit {
       this.forStopName = params.get('stopName');
       return this.busService.arrivals(this.forStopId);
     })).subscribe((response: BustimeResponse) => {
-      if (response.error) {
-        this.error$ = of(response.error);
-      } else {
-        this.vehicles$ = of(response.prd);
-      }
+      this.handleArrivalsResponse(response);
+      timer(this.refreshInterval).pipe(
+        switchMap(() => this.busService.arrivals(this.forStopId))
+      ).subscribe(result => {
+        this.handleArrivalsResponse(result);
+      });
     });
-    // const stopId = 881;
-    // timer(0, 30000).pipe(
-    //   switchMap(() => this.busService.arrivals(stopId))
-    // ).subscribe(result => {
-    //   this.vehicles$ = of(result.prd);
-    // });
+  }
+
+  handleArrivalsResponse(response: BustimeResponse): void {
+    if (response.error) {
+      this.error$ = of(response.error);
+    } else {
+      for (let i = 0; i < response.prd.length; i++) {
+        if (response.prd[0].dly) {
+          response.prd[i].prdctdn = this.getMinutesDifference(
+            response.prd[i].tmstmp,
+            response.prd[i].prdtm);
+        }
+      }
+      this.vehicles$ = of(response.prd);
+    }
+  }
+
+
+  getMinutesDifference(now: string, future: string): string {
+    try {
+      const minutes: string = (((this.getDate(future).getTime() - this.getDate(now).getTime()) / 1000) / 60).toFixed(0);
+      return +minutes > 1 ? minutes : 'DUE';
+    } catch (e) {
+      return 'DLY';
+    }
+  }
+
+  getDate(date: string): Date {
+    const dateTime: string[] = date.split(' ');
+    return new Date(
+      +dateTime[0].slice(0, 4), // Year
+      +dateTime[0].slice(4, 6) - 1, // Month - January is month 0
+      +dateTime[0].slice(6, 8), // Day
+      +dateTime[1].slice(0, 2), // Hour
+      +dateTime[1].slice(3, 5) // Minutes
+    );
   }
 
 }
